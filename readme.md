c2c3e65a825d2f0971a6b88fba9cdfeb97292d3f
Humberto Domínguez <humbertodelac13@gmail.com>
2024-01-30 16:49:44 -0600

    primer socket terminado

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Trabajo con sockets java
+
+> [!IMPORTANT]
+> En este repositorio estoy usando el **JDK en su versión 21**. Algunas de las cosas que estoy usando de esta versión
+> es:
+> * Uso de clase sin nombre
+> * Método main resumido
+
+Más detalles, en la [documentación oficial](https://openjdk.org/jeps/445).
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,69 @@
 //Importaciones necesarias
 
-import java.net.Socket;
-import java.net.InetAddress;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
 
 void main() {
 
+    //declaracion de arreglo con la ip del servidor Echo de internet
+    byte[] byteIp = {52, 43, 121, 77};
+
+    // numero de puerto del servidor
+    int puerto = 9001;
+
+    //objeto de tipo InetAddress
+    InetAddress ip = null;
+    Socket socket = null;
+    BufferedReader sockInput = null;
+    PrintWriter socketOuput = null;
+
+    //manejar la posible excepcion que arroja el metodo getByAddress
+    try {
+        // obteniendo un objeto de tipo InetByAddress
+        ip = InetAddress.getByAddress(byteIp);
+    } catch (UnknownHostException e) {
+        e.printStackTrace(System.out);
+    }
+
+    //manejar la posble excepcion que arroja el constructor Socket
+    try {
+        // creando un socket a partir de la direccion ip
+        socket = new Socket(ip, puerto);
+
+    } catch (IOException e) {
+        e.printStackTrace(System.out);
+    }
+
+    //manejar las posibles excepciones que arrojan los metodos Input y Output Stream
+    try {
+        // objeto que permite leer la respuesta del servidor
+        sockInput = new BufferedReader(
+                new InputStreamReader(socket.getInputStream())
+        );
+
+        //objeto que permite enviar algo al servidor
+        socketOuput = new PrintWriter(socket.getOutputStream(), true);
+    } catch (IOException e) {
+        e.printStackTrace(System.out);
+    }
+
+    //objeto Scanner para leer la consola
+    Scanner consola = new Scanner(System.in);
+
+    String lecturaDeConsola;
+    System.out.println("Escribe lo que sea: ");
+
+    while (true) {
+
+        //mostrar la respuesta del servidor
+        try {
+            System.out.println(sockInput.readLine());
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+        lecturaDeConsola = consola.nextLine();
+        socketOuput.println(lecturaDeConsola);
+    }
 }
